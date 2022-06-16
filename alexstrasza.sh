1897cb2fc89a727a305ab4383f19099e746ee029
AetherBlack <AetherBlack@github.com>
2022-06-16 11:26:04 +0200

    update(bash) : Create temporary directory instead of the whole /tmp

--- alexstrasza.sh
+++ alexstrasza.sh
@@ -4,7 +4,7 @@
 PATH=$(/usr/bin/getconf PATH || kill $$)
 
 # Default
-VOLUME="/tmp"
+VOLUME="$(mktemp -d)"
 
 if [ -n "$1" ]; then
     if [ -d "$1" ]; then
@@ -24,5 +24,6 @@ sudo docker ps -a | grep alexstrasza 1>/dev/null
 if [ $? -eq 0 ]; then
     sudo docker exec -it alexstrasza zsh
 else
+    echo "$VOLUME:/share"
     sudo docker run -it --rm --name alexstrasza --hostname alexstrasza --cap-add SYS_PTRACE --security-opt seccomp=unconfined -v $VOLUME:/share reverse:0.1
 fi
